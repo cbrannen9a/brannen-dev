c855c64fdab2605011ec3b4edf0ee9ca0f6bf0e1
Chris Brannen <cbrannen@outlook.com>
2019-03-01 14:49:26 +0000

    Adding rbx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
+import '../styles/index.css';
+import { Container, Hero, Title } from 'rbx';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
@@ -8,13 +9,16 @@ import SEO from '../components/seo';
 const IndexPage = () => (
 	<Layout>
 		<SEO title='Home' keywords={['gatsby', 'application', 'react']} />
-		<h1>Hi people</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
-		<div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-			<Image />
-		</div>
-		<Link to='/page-2/'>Go to page 2</Link>
+		<Hero color='primary' gradient>
+			<Hero.Body>
+				<Container>
+					<Title>Brannen.dev</Title>
+					<div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+						<Image />
+					</div>
+				</Container>
+			</Hero.Body>
+		</Hero>
 	</Layout>
 );
 
