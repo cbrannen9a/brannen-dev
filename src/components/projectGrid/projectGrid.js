f22c669bb1b330a8c55d578b5906e6c6b0919a60
Chris Brannen <cbrannen@outlook.com>
2019-03-07 15:21:24 +0000

    Adding projects page after limiting project grid on Main

--- src/components/projectGrid/projectGrid.js
+++ src/components/projectGrid/projectGrid.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import { Title } from 'rbx';
-import ProjectPreview from '../projectPreview/projectPreview';
 
-const ProjectGrid = ({ projects }) => {
+import ProjectGridHeader from './projectGridHeader';
+import ProjectGridView from './projectGridView';
+
+const ProjectGrid = ({ projects, isMainPage }) => {
 	return (
 		<div className='project-grid'>
-			<Title className='project-grid-header'>Projects</Title>
-			{
-				projects.map(({ node: project }) => {
-					const title = project.title;
-					const description = project.description;
-					const slug = project.slug.current;
-					const imageData = project.mainImage.asset.fluid;
-					const skills = project.skills;
-
-					return (
-						<ProjectPreview
-							key={title}
-							title={title}
-							description={description}
-							slug={slug}
-							imageData={imageData}
-							skills={skills}
-						/>
-					);
-				})
-			}
+			{isMainPage
+				? <ProjectGridHeader
+					link={'projects'}
+					buttonColour={'link'}
+					buttonTitle={'View All'} />
+				: <ProjectGridHeader
+					link={'/'}
+					buttonColour={'light'}
+					buttonTitle={'Back'} />}
+			<ProjectGridView projects={projects} />
 		</div>);
 };
 
