659f0db13d26f3e0352e326e0170c3494e8123b0
Chris Brannen <cbrannen@outlook.com>
2019-03-08 16:54:56 +0000

    Fixed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,37 +1,51 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { Button, Hero, Title } from 'rbx';
 
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
+
 import '../styles/404.css';
 import Rocket from '../../assets/rocket.svg';
+import Earth from '../../assets/earth.svg';
+import Moon from '../../assets/moon.svg';
 
 const NotFoundPage = () => (
 	<Layout>
 		<SEO title='404: Not found' />
-		<div className='bg-purple'>
-			<h1>NOT FOUND</h1>
-			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-			<div class='objects'>
-				{/* <Rocket className='object_rocket' /> */}
-				{/* <img className='object_rocket' alt='rocket' src='../../assets/rocket.svg' width='40px' /> */}
-				<div className='earth-moon'>
-					<img className='object_earth' alt='earth' src='http://salehriaz.com/404Page/img/earth.svg' width='100px' />
-					<img className='object_moon' alt='moon' src='http://salehriaz.com/404Page/img/moon.svg' width='80px' />
+		<Hero style={{ height: '100vh' }} color='primary' gradient>
+			<Hero.Body>
+				<Title>Lost in space</Title>
+				<Title subtitle>
+					Oh no! Unable to find that planet...
+					<Button
+						rounded
+						style={{ marginLeft: '5px' }}
+						size='small'
+						as={Link}
+						color='link'
+						to={'/'}>
+						Head Home
+					</Button>
+				</Title>
+				<div className='objects'>
+					<Rocket className='rocket' />
+					<Earth className='earth' />
+					<Moon className='moon' />
 				</div>
-				<div className='box_astronaut'>
-					<img className='object_astronaut' alt='astronaut' src='http://salehriaz.com/404Page/img/astronaut.svg' width='140px' />
+				<div className='stars'>
+					<div className='glowing_stars'>
+						<div className='star' />
+						<div className='star' />
+						<div className='star' />
+						<div className='star' />
+						<div className='star' />
+						<div className='star' />
+						<div className='star' />
+					</div>
 				</div>
-			</div>
-			<div className='stars'>
-				<div className='glowing_stars'>
-					<div className='star' />
-					<div className='star' />
-					<div className='star' />
-					<div className='star' />
-					<div className='star' />
-				</div>
-			</div>
-		</div>
+			</Hero.Body>
+		</Hero>
 	</Layout>
 );
 
