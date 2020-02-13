96f76e5c83d8f40c7656dfe68f123a33a4786c17
Chris Brannen <chris.brannen@skybettingandgaming.com>
2020-02-13 21:10:04 +0000

    Fix for casing filename

--- src/components/elements/toolTip/toolTip.js
+++ src/components/elements/toolTip/toolTip.js
@@ -1,30 +1,27 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import '../../../styles/tooltip.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "../../../styles/toolTip.css";
 
 const ToolTip = ({ message, children }) => {
-    const [display, setDisplay] = useState(false);
-    return (
-        <span className='tooltip' onMouseLeave={() => setDisplay(false)}>
-            {display && (
-                <div className={'tooltip-bubble'}>
-                    <div className='tooltip-message'>{message}</div>
-                </div>
-            )}
-            <span
-                className='tooltip-trigger'
-                onMouseOver={() => setDisplay(true)}
-            >
-                {children}
-            </span>
-        </span>
-    );
+  const [display, setDisplay] = useState(false);
+  return (
+    <span className="tooltip" onMouseLeave={() => setDisplay(false)}>
+      {display && (
+        <div className={"tooltip-bubble"}>
+          <div className="tooltip-message">{message}</div>
+        </div>
+      )}
+      <span className="tooltip-trigger" onMouseOver={() => setDisplay(true)}>
+        {children}
+      </span>
+    </span>
+  );
 };
 
 ToolTip.propTypes = {
-    message: PropTypes.oneOfType([PropTypes.element, PropTypes.string])
-        .isRequired,
-    children: PropTypes.element.isRequired
+  message: PropTypes.oneOfType([PropTypes.element, PropTypes.string])
+    .isRequired,
+  children: PropTypes.element.isRequired
 };
 
 export default ToolTip;
