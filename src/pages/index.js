b6adaef0620fb07d03d39d612321fc607413f4db
Chris Brannen <chris.brannen@skybettingandgaming.com>
2020-04-14 12:56:42 +0100

    Removed all firebase.analytics calls

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import { graphql, useStaticQuery } from "gatsby";
-import firebase from "gatsby-plugin-firebase";
 import React from "react";
 import Layout from "../components/layout/layout";
 import PostGrid from "../components/postGrid/postGrid";
@@ -113,7 +112,7 @@ const Home = () => {
     medium: data?.allMediumPost?.edges || [],
     sanity: data.allSanityPost.edges
   });
-  firebase.analytics();
+
   return (
     <Layout>
       <SEO title="Home" />
